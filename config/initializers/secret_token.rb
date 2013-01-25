70f1d7a1a82876491e9732ba6d26cf78a96b3006
renfake <fake.ren@gmail.com>
2013-01-25 21:47:24 +0800

    rails base codes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviePortal::Application.config.secret_token = 'fac058328b37641d27e89503b349e76a9824aa557b3f39ff76feeeaa9f606f99dafec6f586497152f37922d7ed08a09c98e26e85de2d47e55ccdef4766513465'
