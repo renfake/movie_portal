b13bce9ef78cbd220082ba5d4492d7613ad6162c
wdong <dongwzjk@yeah.net>
2013-04-09 12:43:52 +0800

    add score pages and controller.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -9,9 +9,16 @@ class ScoresController < ApplicationController
     #@user = User.find(1)
     #@movie = Movie.find params[:movie_id]
     params[:score].each do |key, value|
-       @score = Score.find_by_item_name_en(key)
-       @user_movie_score = UserMovieScore.new :user_id => 1, :movie_id => params[:movie_id], :score_id => @score.id, :mark => value
-       @user_movie_score.save!
+      @score = Score.find_by_item_name_en(key)
+      @user_movie_score = UserMovieScore.new :user_id => 1, :movie_id => params[:movie_id], :score_id => @score.id, :mark => value
+       #@user_movie_score.save!
+      if @user_movie_score.save
+        flash[:notice] = '添加成功'
+        redirect_to scores_path
+      else
+        flash[:error] = @score.errors
+        render :action => :new
+      end
     end
   end
 end
