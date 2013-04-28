527a5dc8dae59915a413b448f110bd06f39f4ffe
wdong <dongwzjk@yeah.net>
2013-04-28 17:42:13 +0800

    add score pages and controller.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,24 +1,34 @@
+# coding: utf-8
 class ScoresController < ApplicationController
 
-  def new
+  def show
     @movie = Movie.find params[:id]
     @scores = Score.all
+    @user_movie_scores = UserMovieScore.find_by_sql ["SELECT * FROM USER_MOVIE_SCORES WHERE USER_ID = ? AND MOVIE_ID  = ?", 1, params[:id]]
   end
 
   def create
-    #@user = User.find(1)
-    #@movie = Movie.find params[:movie_id]
-    params[:score].each do |key, value|
-      @score = Score.find_by_item_name_en(key)
-      @user_movie_score = UserMovieScore.new :user_id => 1, :movie_id => params[:movie_id], :score_id => @score.id, :mark => value
-       #@user_movie_score.save!
-      if @user_movie_score.save
-        flash[:notice] = '添加成功'
-        redirect_to scores_path
-      else
-        flash[:error] = @score.errors
-        render :action => :new
+    @movie_id = @id = params[:movie_id]
+    if params[:need_update] == true
+      @user_id = 1              #need to be replaced by session
+      params[:score].each do |key, value|
+        @score = Score.find_by_item_name_en(key)
+        @ums = UserMovieScore.find_by_sql ["SELECT * FROM USER_MOVIE_SCORES WHERE USER_ID=? AND MOVIE_ID=? AND SCORE_ID=?", @user_id, @movie_id, @score.id]
+        @ums[0].update_attribute(:mark, value)
       end
+    else
+       params[:score].each do |key, value|
+         @score = Score.find_by_item_name_en(key)
+         @user_movie_score = UserMovieScore.new :user_id => 1, :movie_id => params[:movie_id], :score_id => @score.id, :mark => value
+         @user_movie_score.save!
+       end
     end
+  end
+
+  #查看电影评分结果
+  def edit
+    @movie = Movie.find params[:id]
+    @scores = Score.all
+
   end
 end
